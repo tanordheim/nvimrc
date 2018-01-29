8f0129f1750ef177f7ddf4521b1d6e40915adc0d
Trond Nordheim <trond@nordheim.io>
2018-01-29 15:38:40 +0100

    Only remove Vundle before installing, make sure nvim plugin installation is performed

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ set -x
 BASEDIR=$(pwd -P)
 
 # Prepare the required folders
-rm -rf ~/.vim && mkdir -p ~/.vim/bundle ~/.vim/tmp/undo ~/.vim/tmp/backup ~/.vim/tmp/swap
+mkdir -p ~/.vim/bundle ~/.vim/tmp/undo ~/.vim/tmp/backup ~/.vim/tmp/swap
 
 # Install Vundle.
+rm -rf ~/.vim/bundle/Vundle.vim
 git clone git://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Put the init script in place.
@@ -16,7 +17,7 @@ mkdir -p ~/.config/nvim
 rm -f ~/.config/nvim/init.vim && ln -sf $BASEDIR/init.vim ~/.config/nvim/init.vim
 
 # Install the required plugins with Vundle.
-# nvim +PluginInstall +qall
+nvim +PluginInstall +qall
 
 # Configure YouCompleteMe
 (cd ~/.vim/bundle/YouCompleteMe && ./install.py --cs-completer --go-completer --clang-completer --js-completer)
